87e127582d9ece9b0490878a5c21641d8748aefb
Mark Stahl <mjstahl@gmail.com>
2018-09-11 11:13:27 -0400

    Fix error when missing values but has callback

--- index.js
+++ index.js
@@ -3,28 +3,27 @@
 const path = require('path')
 const { include, renderTemplate } = require('./render')
 
-function __render (content, values, cb, fn) {
-  if (typeof values === 'function') {
-    cb = values
-    values = {}
+function __render (content, values, cb) {
+  if (typeof values === 'function') [cb, values] = [values, {}]
+  return (renderer) => {
+    let promise
+    if (!cb) {
+      promise = new Promise((resolve, reject) => {
+        cb = (err, result) => err ? reject(err) : resolve(result)
+      })
+    }
+    renderer(content, values).then(data => cb(null, data)).catch(cb)
+    return promise
   }
-  let promise
-  if (!cb) {
-    promise = new Promise((resolve, reject) => {
-      cb = (err, result) => err ? reject(err) : resolve(result)
-    })
-  }
-  fn(content, values).then(data => cb(null, data)).catch(cb)
-  return promise
 }
 
-function render (content, values = {}, callback) {
-  return __render(content, values, callback, renderTemplate)
+function render (content, values = {}, cb) {
+  return __render(content, values, cb)(renderTemplate)
 }
 
-function renderFile (file, values = {}, callback) {
+function renderFile (file, values = {}, cb) {
   file = path.isAbsolute(file) ? file : path.join(__dirname, file)
-  return __render(file, values, callback, include)
+  return __render(file, values, cb)(include)
 }
 
 module.exports = { render, renderFile }
